9ada9163e3692927580dfd878038cc9b6157cae4
Lovely Hasan Lucky <lovelyhasanlucky@gmail.com>
2025-05-21 04:21:51 +0600

    The web  app is ready

--- app.py
+++ app.py
@@ -1,30 +1,35 @@
 import pickle
-from flask import Flask,request,app,jsonify,url_for,render_template
+from flask import Flask, request, jsonify, render_template
 import numpy as np
 import pandas as pd
 
-app=Flask(__name__)
-##Load the model
-model=pickle.load(open('regmodel.pkl','rb'))
+app = Flask(__name__)
 
-
-
+# Load the model and scaler
+model = pickle.load(open('regmodel.pkl', 'rb'))
+scaler = pickle.load(open('scaling.pkl', 'rb'))  # Make sure this exists!
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/predict_api',methods=['POST'])
+@app.route('/predict_api', methods=['POST'])
 def predict_api():
-    Data=request.json['data']
+    data = request.json['data']
     print(data)
-    print(np.arraylist(data.values()).reshape(1,-1))
-    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
-    output=regmodel.predict(new_data)
+    arr = np.array(list(data.values())).reshape(1, -1)
+    new_data = scaler.transform(arr)
+    output = model.predict(new_data)
     print(output[0])
     return jsonify(output[0])
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1, -1))
+    print(final_input)
+    output = model.predict(final_input)[0]
+    return render_template("home.html", prediction_text="The House price prediction is {}".format(output))
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
-    
